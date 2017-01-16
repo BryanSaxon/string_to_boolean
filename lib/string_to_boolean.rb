938b646c7d2cfea5bec023007d1709dbd05daf86
Bryan Saxon <emailBryanSaxon@gmail.com>
2017-01-16 12:26:59 -0600

    add code back.

--- lib/string_to_boolean.rb
+++ lib/string_to_boolean.rb
@@ -1,5 +1,13 @@
 require "string_to_boolean/version"
 
-module StringToBoolean
-  # Your code goes here...
+class String
+  def to_bool
+    true_boolean_strings.include?(self)
+  end
+
+  private
+
+  def true_boolean_strings
+    ['true', 'True', 't', 'T', 'yes', 'Yes', 'y', 'Y', '1']
+  end
 end
